1453c77ba4fd9cf0dfaf0c4aa31230a7b39536dd
Agene Sunday <sundayojimagene@gmail.com>
2020-01-16 09:11:06 +0100

    last brushup

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+const spacebutton = document.getElementById('spacebut');
+const texti =document.getElementById('textAri';)
+
+
+
+class Display{
+    constructor(message,time){
+            this.message = message;
+            this.time = time;
+
+    }
+}
+
+function display(){
+    let displayer = new Display('Space school is the best', new Date());
+    return displayer;
+}
+
+    console.log(display);
+
+spacebutton.addEventListe(),function()){
+
+    texti.innerHTML = 'displayer';
+
+};
